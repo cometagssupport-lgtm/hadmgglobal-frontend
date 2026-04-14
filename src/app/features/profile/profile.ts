b16b5b8e6d8bab33e56bfdd2df4681d7d8225ebc
Manikanta Sai <hadmglobal@gmail.com>
2026-04-14 22:56:26 +0530

    trans history

--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -180,7 +180,7 @@ export class Profile implements OnInit {
     } else if (label == 'Set Transaction Password') {
       this.router.navigate(['/set-transaction-password']);
     } else if (label == 'Transaction History') {
-      // this.router.navigate(['/history']);
+      this.router.navigate(['/transactions']);
     }
   }
 
