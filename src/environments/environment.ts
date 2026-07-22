0b91b1816f60591baaa7d52b441df2f126f8f14c
hariprasad <cometagssupport@gmail.com>
2026-07-22 15:09:13 +0530

    update code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api', // your backend base URL
+  apiUrl: 'https://api.cquantadv.com/api', // your backend base URL
   minDepositAmount: 1,
   enablePayloadEncryption: true,
   encryptionSecret: 'hadmg_global_secret_key_2026'
